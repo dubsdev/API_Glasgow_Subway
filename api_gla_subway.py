422147ce0aa7735662844b7eb5354866ff5e41f9
Chris McManus <dubsdevaz@gmail.com>
2021-07-13 00:07:19 +0100

    Added post method for adding stations

--- api_gla_subway.py
+++ api_gla_subway.py
@@ -5,7 +5,7 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-stations = [{'Name': 'bridge street', 'Location': 'Bridge Street'}, {'Name': 'ibrox', 'Location': 'Copland Road'}, {'Name': 'buchanan street', 'Location': 'Well, Buchanan Stree of course'}]
+stations = [{'Name': 'bridge street', 'Location': 'Bridge Street'}, {'Name': 'ibrox', 'Location': 'Copland Road'}, {'Name': 'buchanan street', 'Location': 'Buchanan Street'}]
 
 
 class Index(Resource):
@@ -16,7 +16,23 @@ class All_Stations(Resource):
     def get(self):
         return jsonify(stations)
 
-    def 
+    def post(self):
+
+        req_data=  request.get_json()
+
+        print(type(req_data))
+       
+        new_station = {'Name': req_data['Name'], 'Location': req_data['Location']}
+
+        stations.append(new_station)
+
+        output = jsonify(req_data)
+        output.status_code = 200
+        
+        return f"New station {req_data['Name']} has been added."
+
+
+
 
 class Single_Station(Resource):
     def get(self, station):
@@ -27,16 +43,18 @@ class Single_Station(Resource):
             if station == s['Name']:
                 result.append(s)
         
-        if result == []:
-            return (f'No Station exists with name {station}')
+        #if result == []:
+        #    return (f'No Station exists with name {station}')
         
         return jsonify(result)
 
 
+
 # Add resources
 api.add_resource(Index, '/')
-api.add_resource(All_Stations, '/api/v1/stations/')
+api.add_resource(All_Stations, '/api/v1/stations')
 api.add_resource(Single_Station, '/api/v1/stations/<string:station>')
+
 
 if __name__ == '__main__':
 
