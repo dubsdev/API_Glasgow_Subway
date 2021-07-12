ea11222face1ccdea10868cbe82750e7d0f8c49e
Chris McManus <dubsdevaz@gmail.com>
2021-07-12 17:23:44 +0100

    Added Buchanan Street

--- api_gla_subway.py
+++ api_gla_subway.py
@@ -5,7 +5,7 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-stations = [{'Name': 'bridge street', 'Location': 'Bridge Street'}, {'Name': 'ibrox', 'Location': 'Govan Road'}]
+stations = [{'Name': 'bridge street', 'Location': 'Bridge Street'}, {'Name': 'ibrox', 'Location': 'Govan Road'}, {'Name': 'Buchanan Street'}]
 
 
 class Index(Resource):
